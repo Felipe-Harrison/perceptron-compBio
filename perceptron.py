6bfbca1b3f548f5a4fae47484bbcaa29962abfdc
Felipe-Harrison <harrisufelipe@gmail.com>
2023-10-14 20:06:18 -0300

    Ajustes Finais

--- perceptron.py
+++ perceptron.py
@@ -2,6 +2,10 @@ from random import seed,uniform
 from database import IRIS_SETOSA,IRIS_VIRGINICA,IRIS_VERSICOLOR
 seed(123)
 
+# Configurações 100% -> 
+# [-0.21574777107043808, -0.27576151746860694, 0.7958571366259992, -0.023853694757445573]
+# [-0.40574777107043836, -0.5557615174686068, 1.2258571366259994, -0.043853694757445716]
+# [-0.3457477710704384, -0.9557615174686067, 1.4458571366259996, 0.10614630524255442]
 def generateWeights(nInputs:int) -> list:
     randomWeights = []
     for _ in range(nInputs):
@@ -24,8 +28,8 @@ def activeFunction(value):
         return IRIS_SETOSA
 
 class Perceptron:
-    def __init__(self,nInputs:int) -> None:
-        self.weights = generateWeights(nInputs=nInputs)
+    def __init__(self,nInputs:int,weights = []) -> None:
+        self.weights = generateWeights(nInputs=nInputs) if len(weights) == 0 else weights
         
     def train(self,data,learnRate,epoch):
         
